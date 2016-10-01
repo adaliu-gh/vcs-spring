c962b03a4224f93202b085e499b2aca43d85f9e9
Ada Liu <twostepsfrom_hell@outlook.com>
2016-10-01 19:00:23 +0800

    change package name

---
+++ src/main/java/cn/edu/fudan/ada/vcs-spring/Register.java
@@ -0,0 +1,36 @@
+package cn.edu.fudan.ada.vcsSpring;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Register{
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String cId;
+    private String cName;
+    private String sId;
+    private String sName;
+    private String iId;
+    private String iName;
+    private int score;
+
+    protected Register(){};
+
+    public Register(String cId, String cName, String sId, String sName, String iId, String iName){
+        this.cId=cId;
+        this.cName=cName;
+        this.sId=sId;
+        this.sName=sName;
+        this.iId=iId;
+        this.iName=iName;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Register[id=%d, cId=%s, cName=%s, sId=%s, sName=%s, iId=%s,iName=%s,score=%d]",id,cId,cName,sId,sName,iId,iName,score);
+    }
+}
--- src/main/java/cn/edu/fudan/ada/vcs2/InstructorRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.fudan.ada.vcs2;
-
-import java.util.*;
-import org.springframework.data.repository.CrudRepository;
-
-public interface InstructorRepository extends CrudRepository<Instructor, String>{
-    Instructor findById(String id);
-    List<Instructor> findByName(String name);
-}
