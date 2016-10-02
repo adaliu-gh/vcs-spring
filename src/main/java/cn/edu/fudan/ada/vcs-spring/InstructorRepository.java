d0d163435f675bb60bc4d43af7ccf3b33d902079
Ada Liu <twostepsfrom_hell@outlook.com>
2016-10-02 17:32:46 +0800

    delete old files

--- src/main/java/cn/edu/fudan/ada/vcs-spring/InstructorRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.fudan.ada.vcsSpring;
-
-import java.util.*;
-import org.springframework.data.repository.CrudRepository;
-
-public interface InstructorRepository extends CrudRepository<Instructor, String>{
-    Instructor findById(String id);
-    List<Instructor> findByName(String name);
-}
